5c18d69f50016263847182b06273b17ec62a4b9f
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-03-28 10:25:18 -0500

    Add VideoList and VideoItem components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import youtube from "./apis/youtube";
 
 // Components
 import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
 
 class App extends Component {
   state = {
@@ -21,12 +22,17 @@ class App extends Component {
     this.setState({ videos: res.data.items });
   };
 
+  // Show the video details when a video from the list is clicked
+  onVideoSelect = video => {
+    console.log("From App.js:", video);
+  };
+
   render() {
     const { videos } = this.state;
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        Found {videos.length} videos.
+        <VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
       </div>
     );
   }
---
+++ src/components/VideoItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// Styling
+import "./VideoItem.scss";
+
+const VideoItem = ({ video }) => {
+  return (
+    <div className="item video-item">
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.title}
+      />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoItem;
